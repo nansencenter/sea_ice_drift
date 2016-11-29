7d24924195de04d52d61deff02879110d858c75e
akorosov <korosov@gmail.com>
2016-11-29 08:35:59 +0100

    remove REQS from setup

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 import_error_msg = "Sea Ice Drift requires %s, which should be installed separately"
 
-REQS = ['Nansat', 'opencv']
-
 setup(
     name = "sea_ice_drift",
     version = "0.2",
@@ -17,8 +15,6 @@ setup(
     download_url='https://github.com/nansencenter/sea_ice_drift/archive/v0.2.tar.gz',
     packages=['sea_ice_drift'],
     test_suite="sea_ice_drift.tests",
-    setup_requires=REQS,
-    install_requires=REQS,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
