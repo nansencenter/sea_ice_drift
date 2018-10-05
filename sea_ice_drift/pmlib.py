fcc83a0b744729ddf9caa0ba7f3cfdbc729dccb5
akorosov <korosov@gmail.com>
2018-10-05 10:29:38 +0200

    fix py2 and update readme

--- sea_ice_drift/pmlib.py
+++ sea_ice_drift/pmlib.py
@@ -12,6 +12,8 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+from __future__ import absolute_import, print_function
+
 import time
 from multiprocessing import Pool
 
