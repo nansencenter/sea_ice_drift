d2e985f68f226c92f1dda27f2f98ebe09af97225
Anton Korosov <korosov@gmail.com>
2019-08-27 18:14:35 +0200

    fix minor things

--- provision.sh
+++ provision.sh
@@ -1,17 +1,16 @@
+sudo locale-gen UTF-8
 export VHOME=/home/vagrant
 wget -nv https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $VHOME/miniconda.sh
 chmod +x $VHOME/miniconda.sh
 $VHOME/miniconda.sh -b -f -p $VHOME/miniconda
 export PATH=$VHOME/miniconda/bin:$PATH
 echo "export PATH=$VHOME/miniconda/bin:$PATH" >> $VHOME/.bashrc
-
-conda create --yes -n py3opencv numpy scipy matplotlib pillow netcdf4 gdal opencv notebook
+conda create -c conda-forge --yes -n py3opencv numpy scipy matplotlib pillow netcdf4 gdal=2.4.2 opencv notebook
 source activate py3opencv
 echo "source activate py3opencv" >> $VHOME/.bashrc
 pip install https://github.com/nansencenter/nansat/archive/v1.1.3.tar.gz
 cd /vagrant
 python setup.py install
-
 echo "#!/bin/sh -e
 exec 1>/tmp/rc.local.log 2>&1  # send stdout and stderr from rc.local to a log file
 set -x
